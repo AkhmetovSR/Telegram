df66b9e7588f0e99ef82985167879f05535c2846
AkhmetoSR <ahmetov26@gmail.com>
2024-05-27 09:33:29 +0500

    //

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.css'
+import mix from "./resource/circle.png"
+
+window.Telegram.WebApp.ready(); window.Telegram.WebApp.expand();
+const tg = window.Telegram.WebApp;
+
+function closeApp(){
+  tg.close();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"Main"}>
+          <div className={"ImgDiv"}><img className={"Img"} src={mix} alt="mix"/></div>
+          <div className={"BtnDiv"}><button className={"Btn"} onClick={closeApp}>Закрыть</button></div>
+      </div>
   );
 }
 
 export default App;
+
+
+//($env:HTTPS = "true") -and (npm start)
