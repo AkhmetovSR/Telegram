a8005180483611558fb313e488e36601fca28680
AkhmetoSR <ahmetov26@gmail.com>
2024-06-06 17:49:39 +0500

    //

--- src/GamePage/Claims/Claims.jsx
+++ src/GamePage/Claims/Claims.jsx
@@ -1,16 +1,14 @@
 import s from  "./Claims.module.css"
 import coin from "../../resource/greenCoin.png"
 
-const tg = window.Telegram.WebApp;
-let userName = tg.initDataUnsafe?.user?.username
+function Claims() {
 
-function Claims(props) {
     return (
         <div className={s.Claims}>
-            <div className={s.UserName}>{userName}</div>
+            <div className={s.UserName}></div>
             <div className={s.CoinClaim}>
-                <div className={s.Coin}><img src={coin} className={s.imgCoin}/></div>
-                <div className={s.Claim}>{props.claims}</div>
+                <div className={s.Coin}><img src={coin} className={s.imgCoin} alt="alt"/></div>
+                <div className={s.Claim}></div>
             </div>
         </div>
     );
---
+++ src/TryAgain.jsx
@@ -0,0 +1,11 @@
+import s from "./TryAgain.module.css"
+
+function TryAgain() {
+    return (
+        <div className={s.TryAgain}>
+            TRY AGAIN
+        </div>
+    );
+}
+
+export default TryAgain;
